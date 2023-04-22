7a11e48fb17bf8de7c424b98b49d4337c6b22d22
Antonio Juko <an4037ju-s@student.lu.se>
2023-04-22 19:57:32 +0000

    Update Order.java

--- ICAStoreEJBProjectT4/ejbModule/ics/ICAStoreT4/Order.java
+++ ICAStoreEJBProjectT4/ejbModule/ics/ICAStoreT4/Order.java
@@ -1,5 +1,76 @@
 package ics.ICAStoreT4;
 
-public class Order {
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+
+@Entity
+@NamedQueries({
+    @NamedQuery(name= "Order.findAll", query = "SELECT o FROM Order_ "),
+    @NamedQuery(name = "Store.findByOrderId", query = "SELECT o FROM Order_ s WHERE o.OrderId = :id")
+})
+
+@Table(name = "Store")
+
+public class Order implements Serializable {
+	
+	
+	private int orderId;
+	private String orderDate;
+	private Store store;
+	private Customer customer;
+	private String paymentMethod;
+	
+	@Id
+	@Column (name = "OrderID")
+	public int getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+	@Column (name = "OrderDate")
+	public String getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(String orderDate) {
+		this.orderDate = orderDate;
+	}
+	@ManyToOne
+    @JoinColumn(name = "CustomerID", referencedColumnName = "CustomerID")
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+	
+	@ManyToOne
+    @JoinColumn(name = "StoreID")
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
+	@Column (name = "PaymentMethod")
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+	
+	
+	
 
 }
