c0a6b9c150afca75ba3c150a358bff6f3f723ecd
Nanna Amankwah <na6404am-s@student.lu.se>
2023-04-29 13:09:47 +0200

    "Ny rest servlet"

---
+++ RestServerISPProject/src/main/java/org/ics/ejb/restserver/ICAStore.java
@@ -0,0 +1,59 @@
+package org.ics.ejb.restserver;
+
+import jakarta.ejb.EJB;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import ics.Facade.FacadeLocal;
+
+/**
+ * Servlet implementation class ICAStore
+ */
+public class ICAStore extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@EJB
+	FacadeLocal facade;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ICAStore() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("movies ");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
